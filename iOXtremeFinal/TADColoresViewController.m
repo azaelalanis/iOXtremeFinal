6a778b6b6e7afecf3be7c54c9a55a7a37b4dd3e7
taniags93 <tanita93@gmail.com>
2014-05-12 19:10:33 -0500

    PROYECTO FINAL.
    ya quedó chicos! :D jajaja

--- iOXtremeFinal/TADColoresViewController.m
+++ iOXtremeFinal/TADColoresViewController.m
@@ -1,10 +1,44 @@
-//
-//  TADColoresViewController.m
-//  iOXtremeFinal
-//
-//  Created by Azael Alanis on 3/25/14.
-//  Copyright (c) 2014 ITESM. All rights reserved.
-//
+/*
+ Calentamiento Global para Niños - Consta de Una aplicación móvil para iPad que contiene información sobre el calentamiento
+ global específicamente sobre el cuidado del agua, la biodiversidad, las energías renovables y el reciclaje.
+ Esta aplicación va a estar enfocada en niños de preescolar y el material contenido será fácilmente
+ utilizado por niños de esta edad. Se utilizará la información contenida en la página web de Aprendizaje
+ Verde para Preescolar que existe actualmente y en caso de ser necesario se modificará la presentación
+ de ésta para que pueda ser manipulada por los niños.
+ 
+ Copyright (C) 2014 - ITESM
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ 
+ Authors:
+ 
+ ITESM representatives
+ Ing. Martha Sordia Salinas <msordia@itesm.mx>
+ Dr. Juan Arturo Nolazco Flores <jnolazco@itesm.mx>
+ Ing. Maria Isabel Cabrera Cancino <marisa.cabrera@tecvirtual.mx>
+ 
+ 
+ ITESM students
+ Tania Garrido Salido <taniagarridosalido@gmail.com>
+ Azael Alanis Garza <azaelalanis.g@gmail.com>
+ David de la Fuente Amaya <jdfa14@gmail.com>
+ 
+ 
+ IMAGES COPYRIGHT:
+ (C) PEPSI CO. Aprendizaje Verde Web Page
+ */
 
 #import "TADColoresViewController.h"
 
